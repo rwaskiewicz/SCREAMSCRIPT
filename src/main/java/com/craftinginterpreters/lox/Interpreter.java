5b2603329b3b73dfb02edd5e5f9a76d44f13c73c
ryan.waskiewicz <ryanwaskiewicz@gmail.com>
2019-09-15 11:20:31 -0400

    Complete ch 8

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -105,6 +105,25 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
+  private void executeBlock(List<Stmt> statements, Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
+
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
+    }
+  }
+
+  @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
     Object left = evaluate(expr.left);
     Object right = evaluate(expr.right);
@@ -173,6 +192,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     environment.define(stmt.name.lexeme, value);
     return null;
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+
+    environment.assign(expr.name, value);
+    return value;
   }
 
   @Override
